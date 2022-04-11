b998b8eb7902ef7002452bbb8f11ba3580937fe9
Shawn Pooh <shawn.pooh@yahoo.com>
2022-04-11 08:27:56 -0700

    fixed display issue

--- src/App.js
+++ src/App.js
@@ -43,7 +43,7 @@ function App() {
             sx={{
               background: '#FBF6F0',
               margin: '2rem 0',
-              padding: '0 16px',
+              padding: '0 16px 16px 16px',
               border: '2px solid #D9D7F1',
               borderRadius: '5px',
             }}
